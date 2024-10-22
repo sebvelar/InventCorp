445370410b4de34d974511c4c067f044663e0b01
sebvelar <sebvelar@gmail.com>
2024-10-21 22:51:16 -0600

    Avance de Proyecto

---
+++ InventCorp/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventCorp
+{
+    public abstract class Clientes
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; } 
+        public int Email { get; set; }
+        public string Telefono { get; set; }
+        public int AnioNacimiento { get; set; }
+
+    }
+}
