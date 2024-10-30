3c4b06a4cf23625e01c8c6c8275759c77ad4e9b2
sebvelar <sebvelar@gmail.com>
2024-10-30 13:53:06 -0600

    Se agrego la clase SistemaLogistica

---
+++ InventCorp/IEnvio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventCorp
+{
+    internal interface IEnvio
+    {
+        void AgregarEnvio(Envio envio);
+
+        void BuscarEnvio(string numeroEnvio);
+
+        void ActualizaEstadoEnvio(string numeroEnvio, string nuevoEstado);
+
+        void MostrarTodosLosEnvios();
+
+    }
+}
---
+++ InventCorp/SistemaLogistica.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventCorp
+{
+    class SistemaLogistica : IEnvio
+    {
+
+        public List<Envio> Envios = new List<Envio>();
+
+        public void AgregarEnvio(Envio envio)
+        {
+            Envios.Add(envio);
+            Console.WriteLine($"El envio {envio.NumeroEnvio} con direccion a {envio.Direccion} para el destinatario {envio.Destinatario} se ha enviado correctamente");
+        }
+
+        public void BuscarEnvio(string numeroEnvio)
+        {
+            foreach (Envio thisEnvio in Envios)
+            {
+                if (thisEnvio.NumeroEnvio.Equals(numeroEnvio))
+                {
+                    Console.WriteLine(
+                        "---- Informacion de Envio ----\n\n" +
+                        $"Numero Envio: {thisEnvio.NumeroEnvio} \n" +
+                        $"Destinatario: {thisEnvio.Destinatario} \n" +
+                        $"Direccion: {thisEnvio.Direccion} \n" +
+                        $"Estado: {thisEnvio.Estado}");
+                    return;
+                }
+            }
+            Console.WriteLine("No existe ningun Envio con ese codigo, por favor intente de nuevo");
+        }
+
+        public void ActualizaEstadoEnvio(string numeroEnvio, string nuevoEstado)
+        {
+            foreach (Envio thisEnvio in Envios)
+            {
+                if (thisEnvio.NumeroEnvio.Equals(numeroEnvio))
+                {
+                    thisEnvio.Estado = nuevoEstado;
+                    Console.WriteLine($"El Estado del Envio {thisEnvio.NumeroEnvio} se ha actualizado");
+                }
+            }
+        }
+
+        public void MostrarTodosLosEnvios()
+        {
+            Console.WriteLine("---- Informacion de Envio ----\n\n");
+            foreach (Envio thisEnvio in Envios)
+            {
+                    Console.WriteLine(
+                        $"Numero Envio: {thisEnvio.NumeroEnvio} \n" +
+                        $"Destinatario: {thisEnvio.Destinatario} \n" +
+                        $"Direccion: {thisEnvio.Direccion} \n" +
+                        $"Estado: {thisEnvio.Estado}\n\n");
+            }
+        }
+    }
+}
